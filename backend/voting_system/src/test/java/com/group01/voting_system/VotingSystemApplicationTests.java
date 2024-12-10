f686e7d612ac5065e19645446049fe8cbe901052
Sourav Chandra Dey <dey.sourav0206@gmail.com>
2024-12-10 10:49:04 +0530

    initial project setup

---
+++ backend/voting_system/src/test/java/com/group01/voting_system/VotingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group01.voting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
