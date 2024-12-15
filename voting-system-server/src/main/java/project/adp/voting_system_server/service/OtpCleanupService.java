c4ca7e61955ed75c5d75a291758a4fab19679945
logically-nerd <adnansh2804@gmail.com>
2024-12-15 20:40:43 +0530

    add transactional to otp deletion

--- voting-system-server/src/main/java/project/adp/voting_system_server/service/OtpCleanupService.java
+++ voting-system-server/src/main/java/project/adp/voting_system_server/service/OtpCleanupService.java
@@ -3,6 +3,7 @@ package project.adp.voting_system_server.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import project.adp.voting_system_server.repository.OtpRepository;
 
@@ -16,6 +17,7 @@ public class OtpCleanupService {
 
     // Run every 2 minutes
     @Scheduled(cron = "0 */2 * * * *") // Every 2 minutes
+    @Transactional // Add @Transactional to ensure this method runs within a transaction
     public void deleteExpiredOtps() {
         LocalDateTime expirationThreshold = LocalDateTime.now().minusMinutes(2); // OTPs expire after 2 minutes
         otpRepository.deleteByTimeCreatedBefore(expirationThreshold);
